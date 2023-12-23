89cf5c081bf02597d44293db45d14a3bb6f460b8
parameshwarbhat411 <parameshwar.bhat@gwmail.gwu.edu>
2023-12-23 17:04:02 -0500

    added code to load data onto cloud bucket and transformation code also

--- main.py
+++ main.py
@@ -1,8 +1,10 @@
-
 import etl.extraction as weather
 
 if __name__ == "__main__":
-    locations = ["London", "Tokyo", "Sydney", "Paris", "Berlin", "Moscow", "Madrid", "Rome", "Cairo"]
+    locations = ["London", "Tokyo", "Sydney", "Paris", "Berlin", "Moscow", "Madrid", "Rome", "Cairo","Bangalore"]
     weather = weather.WeatherPipeLine(locations)
-    lat, lon =  weather.extract_lat_lon("Sydney")
-    weather.extract_weather_data(lat,lon)
+    weather.extract_weather_data()
+    weather.load_to_cloudStorage(overwrite=False)
+    df = weather.process_cloud_data().reset_index(drop=True)
+    weather.add_processed_metadata()
+    print("Ready")
